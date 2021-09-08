c568366f1c6f324cb6bf32f81d984d184cb39c2d
Ricardo <vargasr@knights.ucf.edu>
2021-09-08 12:53:16 -0400

    Deleted low resolution images in About page for a cleaner look

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,8 +3,6 @@ import { Container, Row, Col } from "reactstrap";
 
 /* Import images used */
 import headShot from "../assets/img/cerebro_about_4.jpg";
-import cerebroAbout from "../assets/img/cerebro_about_3.jpg";
-import cerebroAbout2 from "../assets/img/cerebro_about_5.jpg";
 import headerImage from "../assets/img/cerebro_1.jpg";
 
 /* Import core components */
@@ -62,43 +60,13 @@ function AboutPage() {
                   Lead Welder
                 </p>
               </Col>
-              <Col className="ml-auto mr-auto text-center" md="10">
+              <Col className="ml-auto mr-auto text-center" md="10" style={{ paddingBottom: "30px" }}>
                 <p className="shadow-sm bg-white rounded description">
                   Gabriel Vargas’s pride and joy are his two kids. He enjoys taking his daughter and son
                   horseback riding and to horse racing events. When Gabriel is not with his kids, he enjoys
                   deer hunting and riding motorbikes up north. Most importantly he enjoys cooking out along
                   with a good tequila, family, and friends.
                 </p>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-        <div className="section-story-overview">
-          <Container>
-            <Row>
-              <Col md="6" style={{ paddingBottom: "30px" }}>
-                <img
-                  src={cerebroAbout}
-                  alt="Cerebro Hunting"
-                  style={{
-                    boxShadow: "0px 10px 25px 0px rgba(0, 0, 0, 0.3)",
-                    borderRadius: "0.1875rem",
-                    maxHeight: "650px",
-                    width: "100%",
-                  }}
-                />
-              </Col>
-              <Col md="6">
-                <img
-                  src={cerebroAbout2}
-                  alt="Cerebro Standing next to Minining Equipment"
-                  style={{
-                    boxShadow: "0px 10px 25px 0px rgba(0, 0, 0, 0.3)",
-                    borderRadius: "0.1875rem",
-                    maxHeight: "650px",
-                    width: "100%",
-                  }}
-                />
               </Col>
             </Row>
           </Container>
