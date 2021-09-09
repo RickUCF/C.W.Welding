6f4c2157ea804f765989b9bceae62b17f7c41ef3
Ricardo <vargasr@knights.ucf.edu>
2021-09-08 22:52:33 -0400

    Remove unecessary useEffect/code

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,31 +20,12 @@ import React from "react";
 import { Container } from "reactstrap";
 
 function Header(props) {
-  let pageHeader = React.createRef();
-
-  /*
-    Resize the header image when window pixal width becomes to small
-  */
-  React.useEffect(() => {
-    if (window.innerWidth > 991) {
-      const updateScroll = () => {
-        let windowScrollTop = window.pageYOffset / 3;
-        pageHeader.current.style.transform = "translate3d(0," + windowScrollTop + "px,0)";
-      };
-      window.addEventListener("scroll", updateScroll);
-      return function cleanup() {
-        window.removeEventListener("scroll", updateScroll);
-      };
-    }
-  });
-
   return (
     <>
       <div className="page-header page-header-small">
         <div
           className="page-header-image"
           style={{ backgroundImage: `url(${props.image})`, backgroundSize: "cover" }}
-          ref={pageHeader}
         ></div>
         <div className="content-center">
           <Container>
